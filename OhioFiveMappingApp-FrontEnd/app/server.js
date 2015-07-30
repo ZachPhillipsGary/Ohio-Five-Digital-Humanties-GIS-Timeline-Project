d012f9b631914a123ab164e0eb2718cfac2a1fdc
zphillipsgarywooster <zphillipsgary@gmail.com>
2015-07-30 17:30:27 -0400

    switched to node.js routing

--- OhioFiveMappingApp-FrontEnd/app/server.js
+++ OhioFiveMappingApp-FrontEnd/app/server.js
@@ -1,11 +1,30 @@
-var express = require('express')
-  , app = express.createServer();
-
-app.use(express.bodyParser());
-
-app.post('/', function(request, response){
-  console.log(request.body);      // your JSON
-  response.send(request.body);    // echo the result back
+/*
+Mapping server. Performs write requests to google drive sheets, serves visitors with the client app.
+Powered by expressjs
+*/
+var express = require('express');
+var app = express(); // init express framework
+/*
+serve files in "client" directory to users
+*/
+app.use(express.static('client'));
+//use jade to serve html files
+app.set('view engine', 'jade');
+//route visitors to client app app
+app.get('/', function (req, res) {
+  res.sendFile('./client/index.html', {root: __dirname })
 });
+/*
+add -- add a marker to a google drive document using node-edit-google-spreadsheet
+*/
+app.post('/add/', function (req, res) {
+  var request = JSON.stringify(req.body));
 
-app.listen(3000);
+
+});
+//init server
+var server = app.listen(8000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+  console.log('Example app listening at http://%s:%s', host, port);
+});
